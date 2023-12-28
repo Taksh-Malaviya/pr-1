f34c196293847770e2bf0fdcac7044a9f565908f
Taksh-Malaviya <150029733+Taksh-Malaviya@users.noreply.github.com>
2023-12-28 12:56:11 +0530

    Update pr-1.c

--- pr-1.c
+++ pr-1.c
@@ -1,3 +1,11 @@
+/*Q.1 Write a Program to convert temperature from degrees Celsius to Fahrenheit.
+Use formula for °C to °F is: ° F = ( °C × 9/5 ) + 32
+For example,
+Input:
+The temperature in Celcius: 38
+
+Output: 
+The temperature in Fahrenheit: 100.4*/
 #include<stdio.h>
 #include<conio.h>
 main()
