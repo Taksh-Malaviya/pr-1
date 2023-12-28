95c93c46b66d055a67246ea48ddc397b8d7475a9
Taksh-Malaviya <150029733+Taksh-Malaviya@users.noreply.github.com>
2023-12-28 12:57:34 +0530

    Update pr-1.3.c

--- pr-1.3.c
+++ pr-1.3.c
@@ -1,3 +1,11 @@
+/*Q.3 Write C Program to find the third angle of triangle if two other angles are given.
+For example,
+Input:
+First angle: 65
+Second angle: 45 
+
+Output: 
+Third angle: 70*/
 #include<stdio.h>
 #include<conio.h>
 void main(){
